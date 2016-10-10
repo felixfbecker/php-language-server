1d5ee25939c61792d8d3754d42b5b6aed4780baf
Felix Becker <felix.b@outlook.com>
2016-10-10 14:12:11 +0200

    Add test for didClose

---
+++ tests/Server/TextDocument/DidCloseTest.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types = 1);
+
+namespace LanguageServer\Tests\Server\TextDocument;
+
+use PHPUnit\Framework\TestCase;
+use LanguageServer\Tests\MockProtocolStream;
+use LanguageServer\{Server, Client, LanguageClient, Project};
+use LanguageServer\Protocol\{TextDocumentItem, TextDocumentIdentifier};
+use Exception;
+
+class DidCloseTest extends TestCase
+{
+    public function test()
+    {
+        $client = new LanguageClient(new MockProtocolStream());
+        $project = new Project($client);
+        $textDocument = new Server\TextDocument($project, $client);
+        $phpDocument = $project->getDocument('whatever');
+        $phpDocument->updateContent('hello world');
+
+        $textDocumentItem = new TextDocumentItem();
+        $textDocumentItem->uri = 'whatever';
+        $textDocumentItem->languageId = 'php';
+        $textDocumentItem->version = 1;
+        $textDocumentItem->text = 'hello world';
+        $textDocument->didOpen($textDocumentItem);
+
+        $textDocument->didClose(new TextDocumentIdentifier($textDocumentItem->uri));
+
+        $this->expectException(Exception::class);
+        $phpDocument->getContent();
+    }
+}
