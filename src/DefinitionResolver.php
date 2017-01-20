0d261db7b4b4fa00bd4a2fb7e2a54b2eee16fb8c
Ivan Bozhanov <github@vakata.com>
2017-01-20 21:33:46 +0200

    fixed coding style

--- src/DefinitionResolver.php
+++ src/DefinitionResolver.php
@@ -423,7 +423,7 @@ class DefinitionResolver
             if ($expr instanceof Node\Expr\Variable && $expr->name === 'this') {
                 $classNode = getClosestNode($expr, Node\Stmt\Class_::class);
                 if ($classNode) {
-                   return self::resolveClassNameToType($classNode->namespacedName);
+                    return self::resolveClassNameToType($classNode->namespacedName);
                 }
                 return new Types\This;
             }
