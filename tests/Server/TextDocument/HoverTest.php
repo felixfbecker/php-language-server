bbb74d1f09cef073148263ab603adc7a39a7c857
Jens Hausdorf <jens1o@users.noreply.github.com>
2017-03-19 11:47:39 +0100

    fix code style 2/2

--- tests/Server/TextDocument/HoverTest.php
+++ tests/Server/TextDocument/HoverTest.php
@@ -23,11 +23,11 @@ class HoverTest extends ServerTestCase
         $this->assertEquals(new Hover([
             new MarkedString('php', "<?php\nclass TestClass implements \\TestInterface"),
             'Pariatur ut laborum tempor voluptate consequat ea deserunt.' . "\n\n" .
-            'Deserunt enim minim sunt sint ea nisi. Deserunt excepteur tempor id nostrud
-laboris commodo ad commodo velit mollit qui non officia id. Nulla duis veniam
-veniam officia deserunt et non dolore mollit ea quis eiusmod sit non. Occaecat
-consequat sunt culpa exercitation pariatur id reprehenderit nisi incididunt Lorem
-sint. Officia culpa pariatur laborum nostrud cupidatat consequat mollit.'
+            'Deserunt enim minim sunt sint ea nisi. Deserunt excepteur tempor id nostrud' . "\n" .
+            'laboris commodo ad commodo velit mollit qui non officia id. Nulla duis veniam' . "\n" .
+            'veniam officia deserunt et non dolore mollit ea quis eiusmod sit non. Occaecat' . "\n" .
+            'consequat sunt culpa exercitation pariatur id reprehenderit nisi incididunt Lorem' . "\n" .
+            'sint. Officia culpa pariatur laborum nostrud cupidatat consequat mollit.'
         ], $reference->range), $result);
     }
 
