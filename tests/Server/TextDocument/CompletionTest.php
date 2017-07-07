2036f3b9582650be295434389984fc02b1b684b7
Ivan Bozhanov <git@vakata.com>
2017-07-07 12:12:18 +0300

    fixed inconsistent code formatting

--- tests/Server/TextDocument/CompletionTest.php
+++ tests/Server/TextDocument/CompletionTest.php
@@ -660,7 +660,7 @@ class CompletionTest extends TestCase
         $this->loader->open($completionUri, file_get_contents($completionUri));
         $items = $this->textDocument->completion(
             new TextDocumentIdentifier($completionUri),
-            new Position(14, 19)
+            new Position(17, 23)
         )->wait();
         $this->assertEquals(new CompletionList([
             new CompletionItem(
