99d8a361db205ee1179d541314b7e0c81d4d06f5
Felix Becker <felix.b@outlook.com>
2017-10-28 14:24:36 -0700

    build: fix typo in release-docker script

--- release-docker.php
+++ release-docker.php
@@ -5,6 +5,6 @@ $dockerUsername = getenv('DOCKER_USERNAME');
 $dockerPassword = getenv('DOCKER_PASSWORD');
 $version = json_decode(file_get_contents(__DIR__ . '/package.json'))->version;
 
-system("docker login -e=$dockerEmail -u=$dockerEmail -p=$dockerPassword");
+system("docker login -e=$dockerEmail -u=$dockerUsername -p=$dockerPassword");
 system("docker build -t felixfbecker/php-language-server:$version .");
 system("docker push felixfbecker/php-language-server:$version");
