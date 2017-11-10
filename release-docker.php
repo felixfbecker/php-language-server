d54ece33669575b56021bf1c199693a7b878fd2e
Felix Becker <felix.b@outlook.com>
2017-11-09 18:59:41 -0800

    build(docker): optimize docker build

--- release-docker.php
+++ release-docker.php
@@ -6,5 +6,7 @@ $dockerPassword = getenv('DOCKER_PASSWORD');
 $version = json_decode(file_get_contents(__DIR__ . '/package.json'))->version;
 
 system("docker login -e=$dockerEmail -u=$dockerUsername -p=$dockerPassword");
-system("docker build -t felixfbecker/php-language-server:$version .");
+system("docker build -t felixfbecker/php-language-server:latest .");
+system("docker tag felixfbecker/php-language-server:latest felixfbecker/php-language-server:$version .");
 system("docker push felixfbecker/php-language-server:$version");
+system("docker push felixfbecker/php-language-server:latest");
