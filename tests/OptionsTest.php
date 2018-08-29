b7bcf00ab3d8f842e34d6e496ab02a444bd130b9
Jürgen Steitz <juergen@steitz.work>
2018-08-29 23:03:25 +0200

    Add basic test

--- tests/OptionsTest.php
+++ tests/OptionsTest.php
@@ -1,10 +1,10 @@
 <?php
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace LanguageServer\Tests;
 
-use PHPUnit\Framework\TestCase;
 use LanguageServer\Options;
+use PHPUnit\Framework\TestCase;
 
 class OptionsTest extends TestCase
 {
@@ -24,5 +24,22 @@ class OptionsTest extends TestCase
         ]);
 
         $this->assertSame($expected, $options->fileTypes);
+    }
+
+    public function testConvertFileSize()
+    {
+        $options = new Options();
+
+        $options->setFileSizeLimit('150K');
+        $this->assertEquals(150000, $options->fileSizeLimit);
+
+        $options->setFileSizeLimit('15M');
+        $this->assertEquals(15000000, $options->fileSizeLimit);
+
+        $options->setFileSizeLimit('15G');
+        $this->assertEquals(15000000000, $options->fileSizeLimit);
+
+        $options->setFileSizeLimit('-1');
+        $this->assertEquals(PHP_INT_MAX, $options->fileSizeLimit);
     }
 }
